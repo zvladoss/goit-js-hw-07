8828ae225905205a315fc8e424e508358407200f
Vladislav Zarovniy <121088289+zvladoss@users.noreply.github.com>
2024-01-16 18:39:12 +0200

    Initial commit

---
+++ js/task-2.js
@@ -0,0 +1,34 @@
+'use strict';
+
+class Storage {
+
+  #items;
+
+  constructor(initialItems) {
+    this.#items = initialItems;
+  }
+
+  getItems() {
+    return this.#items;
+  }
+
+  addItem(newItem) {
+    this.#items.push(newItem);
+  }
+
+  removeItem(itemToRemove) {
+    const itemIndex = this.#items.indexOf(itemToRemove);
+    if (itemIndex !== -1) {
+      this.#items.splice(itemIndex, 1);
+    }
+  }
+}
+
+const storage = new Storage(["Nanitoids", "Prolonger", "Antigravitator"]);
+console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator"]
+
+storage.addItem("Droid");
+console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator", "Droid"]
+
+storage.removeItem("Prolonger");
+console.log(storage.getItems()); // ["Nanitoids", "Antigravitator", "Droid"]
