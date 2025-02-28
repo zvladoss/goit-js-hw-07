0a2a7c651e1b3d3d8fb6ba5d0b8d051bd42fd44f
Vladislav Zarovniy <vladislavzarovnii13@gmail.com>
2025-02-28 18:08:37 +0200

    some-changes

--- js/task-1.js
+++ js/task-1.js
@@ -1,29 +1,14 @@
-const customer = {
-  username: 'Mango',
-  balance: 24000,
-  discount: 0.1,
-  orders: ['Burger', 'Pizza', 'Salad'],
-  // Change code below this line
-  getBalance() {
-    return this.balance;
-  },
-  getDiscount() {
-    return this.discount;
-  },
-  setDiscount(value) {
-    this.discount = value;
-  },
-  getOrders() {
-    return this.orders;
-  },
-  addOrder(cost, order) {
-    this.balance -= cost - cost * this.discount;
-    this.orders.push(order);
-  },
+const refs = {
+  list: document.querySelector('#categories'),
+  items: document.querySelectorAll('#categories .item'),
 };
 
-customer.setDiscount(0.15);
-console.log(customer.getDiscount()); // 0.15
-customer.addOrder(5000, 'Steak');
-console.log(customer.getBalance()); // 19750
-console.log(customer.getOrders()); // ["Burger", "Pizza", "Salad", "Steak"]
+console.log(`Number of categories: ${refs.items.length}`);
+
+refs.items.forEach(item => {
+  const title = item.querySelector('h2').textContent;
+  const elementsCount = item.querySelectorAll('ul li').length;
+
+  console.log(`Category: ${title}`);
+  console.log(`Elements: ${elementsCount}`);
+});
--- js/task-2.js
+++ js/task-2.js
@@ -1,31 +1,43 @@
-class Storage {
-  #items;
-  constructor(params) {
-    this.#items = params;
-  }
-  getItems() {
-    return this.#items;
-  }
+const images = [
+  {
+    url: 'https://images.pexels.com/photos/140134/pexels-photo-140134.jpeg?dpr=2&h=750&w=1260',
+    alt: 'White and Black Long Fur Cat',
+  },
+  {
+    url: 'https://images.pexels.com/photos/213399/pexels-photo-213399.jpeg?dpr=2&h=750&w=1260',
+    alt: 'Orange and White Koi Fish Near Yellow Koi Fish',
+  },
+  {
+    url: 'https://images.pexels.com/photos/219943/pexels-photo-219943.jpeg?dpr=2&h=750&w=1260',
+    alt: 'Group of Horses Running',
+  },
+  {
+    url: 'https://cdn.pixabay.com/photo/2019/05/17/09/27/the-alps-4209272_1280.jpg',
+    alt: 'Alpine Spring Meadows',
+  },
+  {
+    url: 'https://cdn.pixabay.com/photo/2019/05/16/21/10/landscape-4208255_1280.jpg',
+    alt: 'Nature Landscape',
+  },
+  {
+    url: 'https://cdn.pixabay.com/photo/2019/05/17/04/35/lighthouse-4208843_1280.jpg',
+    alt: 'Lighthouse Coast Sea',
+  },
+];
 
-  addItem(newItem) {
-    this.#items.push(newItem);
-  }
+const createGallery = imageInfo => {
+  return `
+    <li class="gallery-item">
+        <img src="${imageInfo.url}" alt="${imageInfo.alt}">
+    </li>
+  `;
+};
 
-  removeItem(itemToRemove) {
-    const itemIndex = this.#items.indexOf(itemToRemove);
-    if (itemIndex !== -1) {
-      this.#items.splice(itemIndex, 1);
-    }
-  }
-}
-const storage = new Storage(['Nanitoids', 'Prolonger', 'Antigravitator']);
-console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator"]
+const galleryCardTemplate = images.map(image => createGallery(image)).join();
+console.log(galleryCardTemplate);
 
-storage.addItem('Droid');
-console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator", "Droid"]
+const refs = {
+  galleryList: document.querySelector('.gallery'),
+};
 
-storage.removeItem('Prolonger');
-console.log(storage.getItems()); // ["Nanitoids", "Antigravitator", "Droid"]
-
-storage.removeItem('Scaner');
-console.log(storage.getItems()); // ["Nanitoids", "Antigravitator", "Droid"]
+refs.galleryList.insertAdjacentHTML('beforeend', galleryCardTemplate);
